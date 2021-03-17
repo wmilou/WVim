9ceaeeafe2f3cf7dc18cff884bb61e48a5acaccf
wedsleyfernando <wedsleyfernando@gmail.com>
2021-03-16 22:57:34 -0300

    Fix output messages

--- install.sh
+++ install.sh
@@ -94,7 +94,7 @@ _git_svn_packs(){
 
 _cfg(){
     echo -e "$COLOR [*] Configuring your vim ... $NC"
-    curl -sLf https://spacevim.org/install.sh | bash -s -- --install neovim
+    curl -sLf https://spacevim.org/install.sh | bash -s -- --install neovim > /dev/null 2>&1
     cp -r $CURRENT_DIR/Configs/SpaceVim ~/.SpaceVim
     cp -r $CURRENT_DIR/Configs/SpaceVim.d ~/.SpaceVim.d
 }
