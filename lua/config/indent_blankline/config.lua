247cccb08f44263fddf920c2432d072413a8296c
wedsleyfernando <wedsleyfernando@gmail.com>
2021-05-26 23:24:32 -0300

    indent line correct context show

--- lua/config/indent_blankline/config.lua
+++ lua/config/indent_blankline/config.lua
@@ -4,4 +4,6 @@ vim.g.indent_blankline_filetype_exclude = {'terminal','dashboard','help'}
 vim.g.indent_blankline_bufname_exclude = {'README.md'}
 vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_show_current_context = true
+vim.g.indent_blankline_context_patterns = {'class', 'function', 'method', 'if', 'else','for', 'foreach','while', 'do'}
+vim.g.indent_blankline_context_highlight_list = {'Warning','Success'}
 vim.g.indent_blankline_show_trailing_blankline_indent = false
