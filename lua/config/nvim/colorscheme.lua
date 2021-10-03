d8844502337e7de856ed328e8be4aa5a894794ca
Wedsley <wedsley.milouchine@vinhasoft.com.br>
2021-10-03 17:31:28 -0300

    material color fix

--- lua/config/nvim/colorscheme.lua
+++ lua/config/nvim/colorscheme.lua
@@ -6,11 +6,38 @@ utils.opt('o', 'termguicolors', true)
 -- vim.cmd 'colorscheme gotham'
 
 -- Material colorscheme
-vim.g.material_style = 'deep ocean'         
-vim.g.material_italic_comments = true
-vim.g.material_italic_keywords = true
-vim.g.material_italic_functions = true
-vim.g.material_italic_variables = false
-vim.g.material_contrast = true
-vim.g.material_borders = false 
-require('material').set()
+--require('material').setup({
+
+	--contrast = true, -- Enable contrast for sidebars, floating windows and popup menus like Nvim-Tree
+	--borders = false, -- Enable borders between verticaly split windows
+
+	--popup_menu = "dark", -- Popup menu style ( can be: 'dark', 'light', 'colorful' or 'stealth' )
+
+	--italics = {
+		--comments = false, -- Enable italic comments
+		--keywords = false, -- Enable italic keywords
+		--functions = false, -- Enable italic functions
+		--strings = false, -- Enable italic strings
+		--variables = false -- Enable italic variables
+	--},
+
+	--contrast_windows = { -- Specify which windows get the contrasted (darker) background
+		--"terminal", -- Darker terminal background
+		--"packer", -- Darker packer background
+		--"qf" -- Darker qf list background
+	--},
+
+	--text_contrast = {
+		--lighter = false, -- Enable higher contrast text for lighter style
+		--darker = false -- Enable higher contrast text for darker style
+	--},
+
+	--disable = {
+		--background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
+		--term_colors = false, -- Prevent the theme from setting terminal colors
+		--eob_lines = false -- Hide the end-of-buffer lines
+	--}
+
+	--custom_highlights = {} -- Overwrite highlights with your own
+--})
+require('material.functions').change_style("deep ocean")
